227df8852084d32c1ba75de827a7f517d5422b53
Antonio <antonioeprado18@gmail.com>
2024-03-19 17:35:34 -0300

    feat: login and logout routes added to route config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '/', component: AppComponent },
+  { path: '/login', component: LoginComponent },
+  { path: '/logout', component: LogoutComponent },
+];
